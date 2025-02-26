b0830877276e3c70bfe87dc44103b57f91b70e80
RafaelPinedaP <rafapineda2807@gmail.com>
2025-02-26 18:36:50 +0100

    Airport completado

---
+++ src/main/java/acme/entities/Airport.java
@@ -0,0 +1,57 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import acme.client.components.basis.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airport extends AbstractEntity {
+
+	@NotBlank
+	@Size(max = 50)
+	private String				name;
+
+	@NotBlank
+	@Column(unique = true, length = 3)
+	@Pattern(regexp = "^[A-Z]{3}$", message = "IATA code must be three uppercase letters")
+	private String				iataCode;
+
+	@Enumerated(EnumType.STRING)
+	private OperationalScope	operationalScope;
+
+	@NotBlank
+	@Size(max = 50)
+	private String				city;
+
+	@NotBlank
+	@Size(max = 50)
+	private String				country;
+
+	@Column(nullable = true)
+	private String				website;
+
+	@Email
+	@Column(nullable = true)
+	private String				email;
+
+	@Pattern(regexp = "^\\+?\\d{6,15}$", message = "Invalid phone number format")
+	@Column(nullable = true)
+	private String				contactPhoneNumber;
+
+
+	public enum OperationalScope {
+		INTERNATIONAL, DOMESTIC, REGIONAL
+	}
+}
