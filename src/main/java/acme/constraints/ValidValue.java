b398ce80d5499647a27566388ce09e2e381d8f06
AlbertoValenzuelaMunoz1 <albvalmunn@alum.us.es>
2025-03-02 13:26:40 +0100

    Validaciones de Airline

---
+++ src/main/java/acme/constraints/ValidValue.java
@@ -0,0 +1,28 @@
+
+package acme.constraints;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({
+	ElementType.FIELD, ElementType.ANNOTATION_TYPE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+
+@Constraint(validatedBy = {
+	ValueValidator.class
+})
+public @interface ValidValue {
+
+	String message() default "{placeholder}";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+	String[] values() default {};
+}
--- src/main/java/acme/entities/Airline.java
+++ src/main/java/acme/entities/Airline.java
@@ -3,9 +3,18 @@ package acme.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidValue;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +23,38 @@ import lombok.Setter;
 @Setter
 public class Airline extends AbstractEntity {
 
-	private String	name;
-	private String	iataCode;
-	private String	website;
-
-	private Date	foundationMoment;
-	private Type	type;
-	private String	email;
-	private String	phoneNumber;
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+	@Automapped
+	@Mandatory
+	@ValidString(max = 50)
+	private String				name;
+	@Column(unique = true)
+	@ValidString(min = 3, max = 3, pattern = "^[A-Z]{2}X$")
+	@Mandatory
+	private String				iataCode;
+	@Automapped
+	@Mandatory
+	@ValidUrl
+	private String				website;
+	@Automapped
+	@Mandatory
+	@ValidMoment(past = true)
+	private Date				foundationMoment;
+	@Automapped
+	@Mandatory
+	@ValidValue(values = {
+		"LUXURY", "STANDARD", "LOW-COST"
+	})
+	private String				type;
+	@Automapped
+	@Optional
+	@ValidEmail
+	private String				email;
+	@Automapped
+	@Optional
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	private String				phoneNumber;
 }
