e7375a882cc815e1e9bfeab56254905ab266e192
guillermorodriguezc <guillerodriguezcarr@gmail.com>
2025-03-10 19:15:30 +0100

    fix: Cambios minimos en la entidad Service

--- src/main/java/acme/entities/Service.java
+++ src/main/java/acme/entities/Service.java
@@ -2,10 +2,13 @@
 package acme.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.PositiveOrZero;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
@@ -22,27 +25,38 @@ import lombok.Setter;
 public class Service extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
+
 	@Mandatory
 	@ValidString(max = 50)
+	@Automapped
 	private String				name;
 
 	@Mandatory
 	@ValidUrl
 	@ValidString(max = 50)
+	@Automapped
 	private String				imageLink;
 
 	@Mandatory
 	@PositiveOrZero
 	@ValidNumber(integer = 3, fraction = 2)
+	@Automapped
 	private Double				averageDwellTime;
 
 	@Optional
 	@ValidScore
 	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$", message = "El código promocional no es válido. Debe ser de 4 letras y 2 dígitos.")
+	@Automapped
 	private String				promotionCode;
 
 	@Optional
-	@ValidMoney
+	@ValidMoney(min = 0)
+	@Automapped
 	private Money				discountAmount;
+
+	@Mandatory
+	@Valid
+	@ManyToOne
+	private Airport				airport;
 
 }
