ddf9703b6019934fb9e55cf581852198463f989b
AntonioRolPe <antoniorolpe@gmail.com>
2025-03-11 08:51:31 +0100

    Anotations for Leg Entity

--- src/main/java/acme/entities/Leg.java
+++ src/main/java/acme/entities/Leg.java
@@ -15,7 +15,12 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Leg extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
