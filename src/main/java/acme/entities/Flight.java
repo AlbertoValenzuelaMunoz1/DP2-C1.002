1d8da3ab92a889dd06b13d153447376eb8ec207d
AntonioRolPe <antoniorolpe@gmail.com>
2025-03-11 09:12:41 +0100

    Anotation for Flight Entity

--- src/main/java/acme/entities/Flight.java
+++ src/main/java/acme/entities/Flight.java
@@ -1,12 +1,15 @@
 
 package acme.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
 
-import acme.client.components.basis.AbstractEntity;
+import org.apache.catalina.Manager;
+import org.dom4j.tree.AbstractEntity;
+
 import acme.client.components.datatypes.Moment;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
@@ -14,7 +17,12 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Flight extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
