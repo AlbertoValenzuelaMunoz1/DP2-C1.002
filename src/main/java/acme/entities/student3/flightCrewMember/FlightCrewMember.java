5479d80268ff99bc0b41c6015d8501d29d0dd950
Fernando Cobos <fercobgar@alum.us.es>
2025-03-12 15:49:12 +0100

    feat: Valid numero de telefono y validator

--- src/main/java/acme/entities/student3/flightCrewMember/FlightCrewMember.java
+++ src/main/java/acme/entities/student3/flightCrewMember/FlightCrewMember.java
@@ -6,12 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
+import acme.Validators.ValidPhoneNumber;
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
-import acme.client.components.validation.ValidPhoneNumber;
 import acme.client.components.validation.ValidString;
 import acme.datatypes.AvailabilityStatus;
 import acme.entities.group.airline.Airline;
