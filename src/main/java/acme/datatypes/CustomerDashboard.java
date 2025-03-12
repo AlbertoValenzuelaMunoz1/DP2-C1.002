1cc9cbcda7f14029d2779ead63a57720b115c3ca
AlbertoValenzuelaMunoz1 <albvalmunn@alum.us.es>
2025-03-12 18:59:29 +0100

    Mejora consulta estadisticas pasageros

--- src/main/java/acme/datatypes/CustomerDashboard.java
+++ src/main/java/acme/datatypes/CustomerDashboard.java
@@ -1,7 +1,6 @@
 
 package acme.datatypes;
 
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -23,7 +22,7 @@ public class CustomerDashboard {
 	private Double					minBookingsCost;
 	private Double					maxBookingsCost;
 	private Double					standardDesviationBookingsCost;
-	private Integer					countBookings;
+	private Long					countBookings;
 
 	private Double					averagePassengers;
 	private Integer					minPassengers;
@@ -37,7 +36,10 @@ public class CustomerDashboard {
 		this.moneySpent = repository.findMoneySpent(customer);
 		this.bookings = repository.findBookingsPerTravelClass(customer).stream().collect(Collectors.toMap(x -> (TravelClass) x[1], x -> (Long) x[0]));
 		this.updateCostStatistics(repository, customer);
-		this.updatePassengerStatistics(repository, customer);
+		this.countBookings = repository.findCountBookingPassenger(customer);
+		this.averagePassengers = repository.findAverageBookingPassenger(customer);
+		this.minPassengers = repository.findMinBookingPassenger(customer);
+		this.maxPassengers = repository.findMaxBookingPassenger(customer);
 	}
 	private void updateCostStatistics(final CustomerRepository repository, final Customer customer) {
 		@SuppressWarnings("deprecation")
@@ -47,14 +49,6 @@ public class CustomerDashboard {
 		this.minBookingsCost = (Double) statistics[2];
 		this.maxBookingsCost = (Double) statistics[3];
 		this.standardDesviationBookingsCost = (Double) statistics[4];
-	}
-	private void updatePassengerStatistics(final CustomerRepository repository, final Customer customer) {
-		List<Long> numberPassengers = repository.findNumberOfPassengersPerBooking(customer);
-		this.countBookings = numberPassengers.size();
-		this.averagePassengers = numberPassengers.stream().mapToDouble(Long::doubleValue).average().orElse(0);
-		this.maxPassengers = numberPassengers.stream().mapToInt(Long::intValue).max().orElse(0);
-		this.minPassengers = numberPassengers.stream().mapToInt(Long::intValue).min().orElse(0);
-		this.standardDesviationPassengers = Math.sqrt(numberPassengers.stream().mapToDouble(c -> Math.pow(c - this.averagePassengers, 2)).average().orElse(0));
 	}
 
 }
--- src/main/java/acme/entities/student2/customer/CustomerRepository.java
+++ src/main/java/acme/entities/student2/customer/CustomerRepository.java
@@ -14,18 +14,17 @@ public interface CustomerRepository extends AbstractRepository {
 	@Query("select sum(b.price.amount) from Booking b where b.customer=:customer")
 	public Double findMoneySpent(final Customer customer);
 	//	@Query("select b from Booking b join b.flight.legs where b.customer=:customer order by ")
-	//	public List<String> findLastDestinations(final Customer customer, Pageable pageable);
+	//	public List<Flight> findLastFlights(final Customer customer, Pageable pageable);
 	@Query("select count(b),b.travelClass from Booking b where b.customer=:customer group by b.travelClass")
 	public List<Object[]> findBookingsPerTravelClass(final Customer customer);
 	@Query("select count(b),avg(b.price.amount),min(b.price.amount),max(b.price.amount),stddev(b.price.amount) from Booking b where b.customer=:customer and year(b.purchaseMoment)>=:minimumYear")
 	public Object[] findCostStatistics(final Customer customer, Integer minimumYear);
-	/*
-	 * No es posible sacar la media, el máximo, el mínimo, etc en la consulta porque
-	 * jpql no admite subconsultas dentro del from, en sql sí sería posible pero para
-	 * seguir las pautas de la asignatura se realizará en jpql y solo se obtendrá el numero
-	 * de pasageros por booking y los cálculos estadísticos se harán una vez obtenida
-	 * la consulta
-	 */
-	@Query("select count(r.booking) from BookingRecord r where r.booking.customer=:customer group by r.booking")
-	public List<Long> findNumberOfPassengersPerBooking(final Customer customer);
+	@Query("select avg(select count(r) from BookingRecord r where r=b) from Booking b where b.customer=:customer")
+	public Double findAverageBookingPassenger(final Customer customer);
+	@Query("select count(select count(r) from BookingRecord r where r=b) from Booking b where b.customer=:customer")
+	public Long findCountBookingPassenger(final Customer customer);
+	@Query("select max(select count(r) from BookingRecord r where r=b) from Booking b where b.customer=:customer")
+	public Integer findMaxBookingPassenger(final Customer customer);
+	@Query("select min(select count(r) from BookingRecord r where r=b) from Booking b where b.customer=:customer")
+	public Integer findMinBookingPassenger(final Customer customer);
 }
