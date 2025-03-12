47eb2db4c4556367915b932e4d523be33d15cc49
AntonioRolpe11 <114919142+AntonioRolpe11@users.noreply.github.com>
2025-03-12 21:45:43 +0100

    fix: Update Manager.java

--- src/main/java/acme/entities/student1/manager/Manager.java
+++ src/main/java/acme/entities/student1/manager/Manager.java
@@ -24,6 +24,8 @@ import lombok.Setter;
 @Setter
 public class Manager extends AbstractRole {
 
+	private static final long	serialVersionUID	= 1L;
+
 	@Mandatory
 	@ValidIdentifier
 	@Column(unique = true)
