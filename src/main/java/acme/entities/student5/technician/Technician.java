6c18b77307bd90132b0173ebad5e562cd38269dd
guillermorodriguezc <guillerodriguezcarr@gmail.com>
2025-03-12 13:14:31 +0100

    fix: Cambios Technician

---
+++ src/main/java/acme/entities/student5/technician/Technician.java
@@ -0,0 +1,54 @@
+
+package acme.entities.student5.technician;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Technician extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				licenseNumber;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				specialization;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				annualHealthTest;
+
+	@Mandatory
+	@ValidNumber(min = 0, integer = 3, fraction = 0)
+	@Automapped
+	private Integer				yearsOfExperience;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				certifications;
+
+}
