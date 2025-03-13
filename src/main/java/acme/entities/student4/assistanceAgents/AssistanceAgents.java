d4d01fc0f78d7b0dc1aa7d381a33bf0c119f3e71
RafaelPinedaP <rafapineda2807@gmail.com>
2025-03-13 17:28:07 +0100

    quick fix

--- src/main/java/acme/entities/student4/assistanceAgents/AssistanceAgents.java
+++ src/main/java/acme/entities/student4/assistanceAgents/AssistanceAgents.java
@@ -9,7 +9,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
@@ -41,8 +40,8 @@ public class AssistanceAgents extends AbstractEntity {
 	private String				employeeCode;
 
 	@Mandatory
-	@NotBlank
 	@ValidString(min = 1)
+	@Automapped
 	private String				spokenLanguages;
 
 	//	@Mandatory
