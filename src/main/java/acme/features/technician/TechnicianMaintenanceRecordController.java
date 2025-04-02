a4c3b5e56e3679a5e3e45077e1b8dc1b029bab41
guillermorodriguezc <guillerodriguezcarr@gmail.com>
2025-04-02 17:24:56 +0200

    Listar todos los maintenance records

---
+++ src/main/java/acme/features/technician/TechnicianMaintenanceRecordController.java
@@ -0,0 +1,20 @@
+
+package acme.features.technician;
+
+import javax.annotation.PostConstruct;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.entities.student5.maintenanceRecord.MaintenanceRecord;
+import acme.entities.student5.technician.Technician;
+
+public class TechnicianMaintenanceRecordController extends AbstractGuiController<Technician, MaintenanceRecord> {
+
+	private TechnicianMaintenanceRecordListService service;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.service);
+	}
+
+}
